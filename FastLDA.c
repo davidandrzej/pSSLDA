66ae14a7a281fdba3f2d4e25324504edaa8e4ca9
David Andrzejewski <andrzeje@cs.wisc.edu>
2010-06-04 17:05:53 -0500

    modify copyright year...

--- FastLDA.c
+++ FastLDA.c
@@ -1,5 +1,5 @@
 /**
-   Copyright (C) 2009 David Andrzejewski (andrzeje@cs.wisc.edu)
+   Copyright (C) 2010 David Andrzejewski (andrzeje@cs.wisc.edu)
  
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
